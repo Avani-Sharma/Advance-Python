b548ac24cf50acb1445f0ed1e796b00b4b2c070e
Avani <avanisharma2405@gmail.com>
2026-05-11 11:38:01 +0530

    programs

--- Pandas/basic.py
+++ Pandas/basic.py
@@ -7,6 +7,7 @@
 # importing pandas
 import pandas as pd
 import numpy as np
+
 arr = np.array([1,2,3,4])
 # 1: series in pandas: it is a data type in pandas which is having one dimension and each of the 
 # elements have the labels that is known as series.
@@ -14,10 +15,21 @@ arr = np.array([1,2,3,4])
 # no. of elements = no. of index if it is not equal it will give an value error 
 a = pd.Series(arr, index=['first', 'second', 'third', 'fourth'])
 print(a)
-
 # indexing
 print(a['first'])
-
 # vectorization is possible
 print(a+10)
+# to check datatype
+print(a.dtype)
+
+print()
+
+# 2: data frame: a two-dimensional, size-mutable, and potentially heterogeneous tabular data structure
+data = {
+    'Name': ['avani', 'chinki'],
+    'Marks': [20, 21],
+    'location': ['Jaipur', 'Delhi']
+}
+print(data)
+print(pd.DataFrame(data, index=['first_stud', 'second_stud']))
 
